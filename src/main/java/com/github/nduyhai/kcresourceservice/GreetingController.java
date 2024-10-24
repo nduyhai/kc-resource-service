77fe41701eb5c081b899c802e45a338b2f91db2d
hai.nguyen <hai.nguyen@itech.asia>
2024-10-24 14:33:10 +0700

    chore: log auth

--- src/main/java/com/github/nduyhai/kcresourceservice/GreetingController.java
+++ src/main/java/com/github/nduyhai/kcresourceservice/GreetingController.java
@@ -1,14 +1,18 @@
 package com.github.nduyhai.kcresourceservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
   @GetMapping
-  public String greeting() {
+  public String greeting(Authentication authentication) {
+    log.info("Found login user {}", authentication);
     return "Greetings from Spring Boot!";
   }
 }
